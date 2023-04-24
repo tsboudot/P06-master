1b5d736c53e5e33b6630d98d796a54d552ac4acf
tsboudot <tsboudot@gmail.com>
2023-04-24 18:28:05 +0200

    utilisateur.js OK / RaF : load HTML + TOKKEN

--- FrontEnd/utilisateur.js
+++ FrontEnd/utilisateur.js
@@ -1,25 +1,26 @@
+let form = document.getElementById('login-form');
 
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let form = document.getElementById('login-form');
-
-    form.addEventListener('submit', async function(event)  {
-      event.preventDefault();
-    try {
-      let response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password
-        })
-      });
-  console.log('response');}
-  catch (error) {
-    console.error('Erreur lors de la requête de login :', error);}
-  });
+form.addEventListener('submit', async function(event)  {
+  event.preventDefault();
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  try {
+    let response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      })
+    });
+    let json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error('Erreur lors de la requête de login :', error);
+  }
+});
       /*if (response.ok) {
         let result = await response.json();
         localStorage.setItem('token', result.token);

994424cb2adf81f386bf8c3d878220d0079fa764
tsboudot <tsboudot@gmail.com>
2023-05-02 18:16:03 +0200

    help Alex

--- FrontEnd/edit_delete.js
+++ FrontEnd/edit_delete.js
@@ -1,5 +1,6 @@
 export async function edit_delete(id) {
     try {
+      console.log('question');
       let response = await fetch(`http://localhost:5678/api/works/delete/${id}`, {
         method: 'DELETE',
         headers: {
@@ -7,14 +8,16 @@ export async function edit_delete(id) {
         },
       });
       let data = await response.json();
+      console.log('reponse');
       return data;
     } catch (error) {
       console.error('Error:', error);
+    
     }
   }
   
   export function edit_delete_sign(id, container) {
-    const delete_sign = document.createElement('div');
+    const delete_sign = document.createElement('button');
     delete_sign.innerHTML = '<i class="fa-light fa-trash"></i>';
     delete_sign.addEventListener('click', () => edit_delete(id));
     container.appendChild(delete_sign);
